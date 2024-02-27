1d7b2075aafd4e4951035d2ec653e40471ec8174
SyedRaheelShah <160128463+SyedRaheelShah@users.noreply.github.com>
2024-02-28 03:27:44 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+//Question1
+console.log("Hello World");
+//Question2
+var myName = 'Raheel';
+var message = 'is programmer';
+console.log(myName + " " + message);
---
+++ main.ts
@@ -0,0 +1,9 @@
+//Question1
+console.log("Hello World");
+
+
+//Question2
+let myName = 'Raheel';
+let message = 'is programmer'
+console.log(myName+" "+message);
+
